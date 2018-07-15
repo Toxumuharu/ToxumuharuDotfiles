074287b2dc5b65dc6d6bf88f79d30cee8e98214c
Toxumuharu <ring.o.awlright@gmail.com>
2018-07-16 02:28:48 +0900

    added installer

--- installer.sh
+++ installer.sh
@@ -1,17 +1,14 @@
-#!/bin/sh
+﻿#!/bin/sh
 
 case "${OSTYPE}" in
 	darwin*)
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	;;
 	linux*)
 	sudo apt update -y
 	sudo apt upgrade -y
 	sudo apt install build-essential devscripts -y
 	sudo apt install chromium-browser -y
-	sudo apt install byobu -y
-	;;
+	sudo apt install byobu -y;;
 esac
 
-ln -sf ~/ToxumuharuDotfiles/.vimrc ~/.vimrc
-ln -sf ~/ToxumuharuDotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/ToxumuharuDotfiles/.bashrc ~/.bashrc
