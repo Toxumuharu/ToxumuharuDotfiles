ad9582c9bccdd887510d71cbe809f60dcf59a23f
Toxumuharu <ring.o.awlright@gmail.com>
2018-07-16 02:09:48 +0900

    changed installer

--- installer.sh
+++ installer.sh
@@ -4,9 +4,6 @@ case "${OSTYPE}" in
 	darwin*)
 	;;
 	linux*)
-	sudo su -c "grep '^deb ' /etc/apt/sources.list | \
-	sed 's/^deb/deb-src/g' > /etc/apt/sources.list.d/deb-src.list"
-	
 	sudo apt update -y
 	sudo apt upgrade -y
 	sudo apt install build-essential devscripts -y
