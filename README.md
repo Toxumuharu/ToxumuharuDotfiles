250ab7f55099439050687695d4d1f36d866c5eef
Toxumuharu <ring.o.awlright@gmail.com>
2018-07-16 01:42:04 +0900

    20180716

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ToxumuharuDotfiles
+
+this file is created by Toxumuharu, just for any kinds of bash system.
---
+++ dotfilesLink.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/sh
+
+case "${OSTYPE}" in
+	darwin*)
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	;;
+	linux*)
+	sudo apt install -y vim
+	;;
+esac
+
+ln -sf ~/ToxumuharuDotfiles/.vimrc ~/.vimrc
+ln -sf ~/ToxumuharuDotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/ToxumuharuDotfiles/.bashrc ~/.bashrc
