190bd25a831eccdfed146064bc3ea9c2d32aeb87
Toxumuharu <ring.o.awlright@gmail.com>
2018-07-19 04:33:05 +0900

    added Toxumuharu(笑)

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # ToxumuharuDotfiles
 
 this file is created by Toxumuharu, just for any kinds of bash system.
+i can not assume the responsibility at any time.
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
 case "${OSTYPE}" in
-	darwin*)
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	brew install vim -y
-	export PATH="/usr/local/bin:$PATH"	
-	brew install ack -y
-	;;
-	linux*)
-	sudo add-apt-repository ppa:dawidd0811/neofetch -y
-	sudo apt update -y
-	sudo apt install neofetch -y
-	sudo apt install curl -y
-	sudo apt install vim -y
+darwin*)
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" -y
+brew install vim -y
+export PATH="/usr/local/bin:$PATH"
+cat ./toxumuharu.txt
+brew install ack -y
+;;
+linux*)
+sudo add-apt-repository ppa:dawidd0811/neofetch -y
+sudo apt update -y
+sudo apt install neofetch -y
+cat ./toxumuharu.txt
+sudo apt install curl -y
+sudo apt install vim -y
+sudo apt install ack-grep -y
 string="Microsoft"
 if test $(uname -r | sed -n 's/.*\(Microsoft *\).*/\1/p') = $string
 then
-	echo "WSL detected"
-	neofetch
-	else
-	echo "Linux detected"
-	neofetch
-	sudo apt install build-essential devscripts -y
-	sudo apt install chromium-browser -y
-	sudo apt install byobu -y
-	fi
-	;;
+echo "WSL detected"
+neofetch
+else
+echo "Linux detected"
+neofetch
+sudo apt install build-essential devscripts -y
+sudo apt install chromium-browser -y
+sudo apt install byobu -y
+fi
+;;
 esac
 
 ln -sf ~/ToxumuharuDotfiles/.vimrc ~/.vimrc
