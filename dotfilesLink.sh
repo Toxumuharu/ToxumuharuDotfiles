793b793bbb6b674baa52191d2727dc3dd509ff90
Toxumuharu <ring.o.awlright@gmail.com>
2018-07-19 03:58:34 +0900

    added detect OS system

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,17 +5,26 @@ case "${OSTYPE}" in
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	brew install vim -y
 	export PATH="/usr/local/bin:$PATH"	
+	brew install ack -y
 	;;
 	linux*)
+	sudo add-apt-repository ppa:dawidd0811/neofetch -y
+	sudo apt update -y
+	sudo apt install neofetch -y
+	sudo apt install curl -y
+	sudo apt install vim -y
 string="Microsoft"
 if test $(uname -r | sed -n 's/.*\(Microsoft *\).*/\1/p') = $string
 then
-echo "WSL detected"
-else
-echo "Linux detected"
-fi
-	sudo apt install curl -y
-	sudo apt install vim -y
+	echo "WSL detected"
+	neofetch
+	else
+	echo "Linux detected"
+	neofetch
+	sudo apt install build-essential devscripts -y
+	sudo apt install chromium-browser -y
+	sudo apt install byobu -y
+	fi
 	;;
 esac
 
