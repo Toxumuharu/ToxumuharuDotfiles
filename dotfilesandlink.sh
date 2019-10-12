bfda7912411bdfbc84440dbd6db562840588afd5
Toxumuharu <ring.o.awlright@gmail.com>
2019-10-12 21:33:29 +0900

    remove google drive installing

--- dotfilesandlink.sh
+++ dotfilesandlink.sh
@@ -42,8 +42,8 @@ sudo apt install software-properties-common dirmngr gnupg-agent
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AD5F235DF639B041
 echo 'deb http://ppa.launchpad.net/alessandro-strada/ppa/ubuntu xenial main' | sudo tee /etc/apt/sources.list.d/alessandro-strada-ubuntu-ppa.list >/dev/null
 sudo apt update
-sudo apt install google-drive-ocamlfuse
-mkdir ~/GoogleDrive
+#sudo apt install google-drive-ocamlfuse
+#mkdir ~/GoogleDrive
 fi
 
 #OS type == Windows Subsystem for Linux(WSL)
